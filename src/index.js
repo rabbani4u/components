2075cb223da13ccad9932d470e82f053cb92d642
rabbani4u <rabbani.iu31@gmail.com>
2022-11-27 12:52:12 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Heading(){
+  return <h1>My Favourite Foods</h1>
+}
 
-ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
+function List(){
+  <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
     </ul>
+}
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
