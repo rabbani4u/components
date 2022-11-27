71c49b17265a512f401690dd54d23df8b7c5037b
rabbani4u <rabbani.iu31@gmail.com>
2022-11-27 13:07:27 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Heading(){
-  return <h1>My Favourite Foods</h1>
-}
-
-function List(){
-  return(
-  <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  )
-}
+import App from "./components/App"
 
 ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
+  <App />,
   document.getElementById("root")
 );
